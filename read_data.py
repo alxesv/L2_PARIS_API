6158c4c0a8fed12233bef40df68c1e69abe02f42
mguillemin <mguillemin@roole.fr>
2023-10-09 16:40:47 +0200

    add simple get method

--- read_data.py
+++ read_data.py
@@ -1,17 +1,16 @@
-import datetime
-from typing import Union
+import json
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
-
-@app.get("/itemsjson/{date}")
-def read_data(date: datetime.date,tmin: int,tmax: int,prc: float,snow:float,snwd:float,awnd:float):
+@app.get('')
+def read_json():
     """
     ### Retour
     - return un ensemble de données de la date
     mise en paramètre de l'url a la place de {date}
     """
-    return {"date": date, "tmin": tmin,"tmax":tmax,"prc":prc,"snow":snow,"snwd":snwd,"awnd":awnd,}
-
+    with open("rdu-weather-history.json", "r") as f:
+        data = json.load(f)
+    return data
--- router/weather.py
+++ router/weather.py
@@ -1,3 +1,14 @@
 from fastapi import APIRouter
-
+from fastapi import FastAPI
+import json
 router = APIRouter(prefix="/weather", tags=['weather'])
+
+@router.get('')
+def read_json():
+    """
+    ### Return with this method
+    - returns a set of weather data without parameters in the url
+    """
+    with open("rdu-weather-history.json", "r") as f:
+        data = json.load(f)
+    return data

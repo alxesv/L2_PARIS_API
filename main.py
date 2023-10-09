274feb6cd59d6d0dcb513ef0040c7bb759da70ac
alex <alexandre.souvorov@edu.esiee-it.fr>
2023-10-09 15:17:01 +0200

    example docstring

--- main.py
+++ main.py
@@ -12,4 +12,10 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
+    """
+    Exemple de docstring pour la fonction read_item
+    :param item_id: id de l'item
+    :param q: paramètre optionnel
+    :return: dict
+    """
     return {"item_id": item_id, "q": q}

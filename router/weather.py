bc53a69987ed88ea629083968421213ec42e0df4
Alexandre <116552630+Creepynax@users.noreply.github.com>
2023-10-10 09:19:54 +0200

    modification de la route vers le fichier put.py

--- router/weather.py
+++ router/weather.py
@@ -4,4 +4,4 @@ router = APIRouter(prefix="/weather", tags=['weather'])
 from router.methods.create import create_weather
 from router.methods.read_data import read_json
 from router.methods.patch import modify_weather
-from put import put_weather
+from router.methods.put import put_weather

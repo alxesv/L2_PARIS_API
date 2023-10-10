500dd31b205d24d8307949d6ce627e0d7b613aed
Alexandre <116552630+Creepynax@users.noreply.github.com>
2023-10-10 09:19:03 +0200

    fichier put.py fini

--- router/weather.py
+++ router/weather.py
@@ -1,3 +1,7 @@
 from fastapi import APIRouter
 
 router = APIRouter(prefix="/weather", tags=['weather'])
+from router.methods.create import create_weather
+from router.methods.read_data import read_json
+from router.methods.patch import modify_weather
+from put import put_weather

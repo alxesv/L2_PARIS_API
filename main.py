72d8dd73cf7bbb16cfc8d3b594c25fed4378e46f
Branlii <guillot.raphael11@gmail.com>
2023-10-11 10:42:31 +0200

    create jwt token via auth route

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
 from router.unite.unite import router as unite_router
+from router.authentification.authentification import router as authentification_router
 
 from jose import jwt
 
@@ -21,31 +22,35 @@ async def verify_token(request: Request, call_next):
     request param = details on the request
     call_next     = call the function next the middleware
     """
-    # don't verify on route /jwt/
-    if request.url.path == "/jwt/":
-        return await call_next(request)
-    # try to decode the token if there's one
-    if request.headers.get('authorization') is not None:
-        try:
-            jwt.decode(request.headers.get('authorization'), os.getenv('JWT_SECRET'), algorithms=['HS256'])
-        except Exception as e:
+    if request.url.path.startswith("/api"):
+        # don't verify on route /jwt/
+        if request.url.path == "/api/auth/jwt":
+            return await call_next(request)
+        # try to decode the token if there's one
+        if request.headers.get('authorization') is not None:
+            try:
+                jwt.decode(request.headers.get('authorization'), os.getenv('JWT_SECRET'), algorithms=['HS256'])
+            except Exception as e:
+                return_message = {
+                    "status": 403,
+                    "message": "Token de connection invalide",
+                    "reason": str(e)
+                }
+                return JSONResponse(status_code=403, content=return_message)
+        else:
             return_message = {
-                "status": 403,
-                "message": "Token de connection invalide",
-                "reason": str(e)
+                "status": 401,
+                "message": "Aucun token de connexion envoyé",
             }
-            return JSONResponse(status_code=403, content=return_message)
+            return JSONResponse(status_code=401, content=return_message)
+        response = await call_next(request)
+        return response
     else:
-        return_message = {
-            "status": 401,
-            "message": "Aucun token de connexion envoyé",
-        }
-        return JSONResponse(status_code=401, content=return_message)
-    response = await call_next(request)
-    return response
+        return await call_next(request)
 
 
-app.include_router(unite_router)
+app.include_router(unite_router, prefix="/api")
+app.include_router(authentification_router, prefix="/api")
 
 
 @app.get("/")

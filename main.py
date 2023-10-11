803f17186608acb795cc4778d387e489076069f1
Branlii <guillot.raphael11@gmail.com>
2023-10-11 08:21:24 +0200

    add jwt middleware to verify http incoming request

--- main.py
+++ main.py
@@ -1,11 +1,52 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
 from router.unite.unite import router as unite_router
 
+from jose import jwt
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 app = FastAPI()
+
+
+@app.middleware("http")
+async def verify_token(request: Request, call_next):
+    """
+    used to verify authorization from an http request
+    request param = details on the request
+    call_next     = call the function next the middleware
+    """
+    # don't verify on route /jwt/
+    if request.url.path == "/jwt/":
+        return await call_next(request)
+    # try to decode the token if there's one
+    if request.headers.get('authorization') is not None:
+        try:
+            jwt.decode(request.headers.get('authorization'), os.getenv('JWT_SECRET'), algorithms=['HS256'])
+        except Exception as e:
+            return_message = {
+                "status": 403,
+                "message": "Token de connection invalide",
+                "reason": str(e)
+            }
+            return JSONResponse(status_code=403, content=return_message)
+    else:
+        return_message = {
+            "status": 401,
+            "message": "Aucun token de connexion envoyé",
+        }
+        return JSONResponse(status_code=401, content=return_message)
+    response = await call_next(request)
+    return response
+
+
 app.include_router(unite_router)
+
 
 @app.get("/")
 def read_root():

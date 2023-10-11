5507f20d009ed5a87c21be498b1259b7a5743474
alex <alexandre.souvorov@edu.esiee-it.fr>
2023-10-11 09:37:49 +0200

    Delete unite

---
+++ router/unite/methods/delete.py
@@ -0,0 +1,21 @@
+from database import session
+from router.unite.unite import router
+from models import Unite
+
+@router.delete("/{unite}")
+def delete_unite(unite: str):
+    """
+    ### Paramètres
+    - unite: le nom de l'unite
+    ### Retour
+    - un message de confirmation ou d'erreur
+    - un status code correspondant
+    """
+    unites = session.query(Unite).all()
+    for un in unites:
+        if un.un == unite:
+            session.delete(un)
+            session.commit()
+            return {"message": "Unite supprimée avec succès", "status": 200}
+
+    return {"message": "Unite non trouvée", "status": 404}
--- router/unite/unite.py
+++ router/unite/unite.py
@@ -3,3 +3,4 @@ from fastapi import APIRouter
 router = APIRouter(prefix="/unite", tags=['unite'])
 from .methods.create import *
 from .methods.read import *
+from .methods.delete import *

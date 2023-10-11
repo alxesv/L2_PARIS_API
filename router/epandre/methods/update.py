c83a25510347beca00a86017bfeae10f8bf736f0
alex <alexandre.souvorov@edu.esiee-it.fr>
2023-10-11 15:57:04 +0200

    wip epandre update

--- router/epandre/methods/update.py
+++ router/epandre/methods/update.py
@@ -2,3 +2,22 @@ from database import session
 from router.epandre.epandre import router
 from models import Epandre
 from fastapi import HTTPException
+from pydantic import BaseModel
+
+class EpandreBase(BaseModel):
+    id_engrais: int
+    no_parcelle: int
+    date: str
+    qte_epandue: int = None
+
+@router.patch('/', status_code=200)
+def modify_epandre(epandre_id: EpandreBase):
+    """
+    Modifie une ligne dans la table epandre
+    ### Paramètres
+    - epandre_id: objet de type Epandre, avec les champs id_engrais, no_parcelle et date
+    ### Retour
+    - un message de confirmation ou d'erreur
+    - un status code correspondant
+    """
+    pass

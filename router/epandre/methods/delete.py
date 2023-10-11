27e6d9aff52fe9f2626aeeb12b2c4764617e4085
alex <alexandre.souvorov@edu.esiee-it.fr>
2023-10-11 16:59:46 +0200

    change delete method from body to query params

--- router/epandre/methods/delete.py
+++ router/epandre/methods/delete.py
@@ -2,15 +2,10 @@ from database import session
 from router.epandre.epandre import router
 from models import Epandre
 from fastapi import HTTPException
-from pydantic import BaseModel
 from models import Date
 
-class EpandrePrimary(BaseModel):
-    id_engrais: int
-    no_parcelle: int
-    date: str
 @router.delete("/", status_code=200)
-def delete_epandre(epandre_id: EpandrePrimary):
+def delete_epandre(id_engrais: int = None, no_parcelle: int = None, date: str = None):
     """
     Supprime une ligne dans la table epandre
     ### Paramètres
@@ -19,19 +14,19 @@ def delete_epandre(epandre_id: EpandrePrimary):
     - un message de confirmation ou d'erreur
     - un status code correspondant
     """
-    if epandre_id.id_engrais is None or epandre_id.no_parcelle is None or epandre_id.date is None:
+    if id_engrais is None or no_parcelle is None or date is None:
         raise HTTPException(status_code=400, detail="Il manque au moins un paramètre")
 
     dates = session.query(Date).all()
-    if not any(date.date == epandre_id.date for date in dates):
+    if not any(orm_date.date == date for orm_date in dates):
         raise HTTPException(status_code=404, detail="Date non trouvée")
     else:
-        date_object = session.query(Date).filter(Date.date == epandre_id.date).first()
+        date_object = session.query(Date).filter(Date.date == date).first()
 
     epandres = session.query(Epandre).all()
     for epandre in epandres:
-        if epandre.id_engrais == epandre_id.id_engrais and epandre.no_parcelle == epandre_id.no_parcelle and epandre.date == date_object:
-            deleted_epandre = epandre_id.model_dump()
+        if epandre.id_engrais == id_engrais and epandre.no_parcelle == no_parcelle and epandre.date == date_object:
+            deleted_epandre = epandre
             session.delete(epandre)
             session.commit()
             return {"message": "Epandre supprimé avec succès", "deleted_epandre": deleted_epandre}

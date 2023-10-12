e56fd6c9c13acebda5b98f88a3fb986899d355e1
Alexandre <116552630+Creepynax@users.noreply.github.com>
2023-10-13 00:27:32 +0200

    review culture terminée

--- router/culture/methods/create.py
+++ router/culture/methods/create.py
@@ -6,7 +6,6 @@ from models import Parcelle, Production, Culture
 from fastapi import HTTPException, status
 from pydantic import BaseModel
 
-
 class CultureBase(BaseModel):
     no_parcelle: int
     code_production: int
@@ -14,28 +13,23 @@ class CultureBase(BaseModel):
     date_fin: str
     qte_recoltee: int
 
-
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def create_culture(new_culture: CultureBase):
     """
-   Ajoute une ligne dans la table unite
+    Ajoute une ligne dans la table Culture
     ### Paramètres
-    - no_parcelle : Numéro de la parcelle attribué à cette culture
-    - code_production : Code de la production attribué à cette culture
-    - date_debut : Date du début de la culture
-    - date_fin : Date de la fin de la culture
-    - qte_recoltee : Quantitée récolté provenant de la culture
+    - new_culture: objet de type Culture, avec les champs no_parcelle, code_production, date_debut, date_fin, qte_recoltee
     ### Retour
     - Status code 201 si tout s'est bien passé avec message de confirmation
     - Message d'erreur avec le status code correspondant sinon
     """
     parcelles = session.query(Parcelle).all()
     if not any(parcelle.no_parcelle == new_culture.no_parcelle for parcelle in parcelles):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Numéro de parcelle non trouvée")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Aucun numéro de parcelle trouvé")
 
     productions = session.query(Production).all()
     if not any(production.code_production == new_culture.code_production for production in productions):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Code de production non trouvée")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Aucun code de production trouvé")
 
     date_debut = datetime.strptime(new_culture.date_debut, "%Y-%m-%d")
     date_fin = datetime.strptime(new_culture.date_fin, "%Y-%m-%d")
@@ -46,7 +40,7 @@ def create_culture(new_culture: CultureBase):
         culture = Culture(**new_culture.__dict__)
         session.add(culture)
         session.commit()
-        return {"message": "Culture créé avec succès", "element": new_culture}
+        return {"message": "Culture créé avec succès", "new_culture": new_culture}
 
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
--- router/culture/methods/read.py
+++ router/culture/methods/read.py
@@ -7,21 +7,27 @@ from models import Culture, Parcelle, Production
 from sqlalchemy import asc, desc
 from fastapi import HTTPException, status
 
-
 @router.get("/", status_code=status.HTTP_200_OK)
 def read_cultures(skip: int = 0, limit: int = 10, sort: str = None, no_parcelle: int = None, code_production: int = None, date_debut: str = None, date_fin: str = None, qte_recoltee: int = None):
     """
-    Récupère les lignes de la table élément chimique
+    Récupère les lignes de la table Culture
     ### Paramètres
     - skip: nombre d'éléments à sauter
     - limit: nombre d'éléments à retourner
+    - sort: le ou les champs sur lequel trier les résultats
+    - no_parcelle : Numéro de la parcelle attribué à cette culture à filtrer
+    - code_production : Code de la production attribué à cette culture à filtrer
+    - date_debut : Date du début de la culture à filtrer
+    - date_fin : Date de la fin de la culture à filtrer
+    - qte_recoltee : Quantitée récolté provenant de la culture à filtrer
     ### Retour
-    - un tableau d'objets de type Culture
+    - un objet JSON contenant  les lignes de la table Culture, filtrées et/ou triées
     - un message d'erreur en cas d'erreur
     - un status code correspondant
+    - url de navigation pour la pagination
     """
 
-    url = f"http://127.0.0.1:8000/culture?"
+    url = f"http://127.0.0.1:8000/api/culture?"
 
     sort_mapping = Culture.__table__.columns.keys()
 
@@ -76,7 +82,7 @@ def read_cultures(skip: int = 0, limit: int = 10, sort: str = None, no_parcelle:
             # First exception is already raised, so do nothing
             pass
         except:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Parcelle non existante")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Aucune parcelle existante")
 
         data = [culture for culture in data if culture.no_parcelle == parcelle_object.no_parcelle]
 
@@ -89,7 +95,7 @@ def read_cultures(skip: int = 0, limit: int = 10, sort: str = None, no_parcelle:
             # First exception is already raised, so do nothing
             pass
         except:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Production non existante")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Aucune production existante")
 
         data = [culture for culture in data if culture.code_production == production_object.code_production]
 
@@ -105,7 +111,7 @@ def read_cultures(skip: int = 0, limit: int = 10, sort: str = None, no_parcelle:
     if url[-1] != "?":
         url += "&"
 
-    response = { "cultures": [culture for culture in data[skip:skip + limit]] }
+    response = {"cultures": [culture for culture in data[skip:skip + limit]]}
 
     if skip + limit < len(data):
         response["nextPage"] = f"{url}skip={str(skip + limit)}&limit={str(limit)}"
@@ -116,9 +122,9 @@ def read_cultures(skip: int = 0, limit: int = 10, sort: str = None, no_parcelle:
 
 
 @router.get("/{identifiant_culture}", status_code=status.HTTP_200_OK)
-def read_culture(identifiant_culture: int):
+def read_culture_by_identifiant_culture(identifiant_culture: int):
     """
-    Récupère une ligne de la table élément chimique
+    Récupère une ligne de la table Culture
     ### Paramètres
     - identifiant_culture: Identifiant de la culture voulue
     ### Retour
@@ -133,4 +139,4 @@ def read_culture(identifiant_culture: int):
     if not data:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Culture introuvable")
 
-    return { "culture": data }
+    return {"culture": data}

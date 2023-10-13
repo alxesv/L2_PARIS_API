7a36f4c630bf3594b3573dc55a11a44a98e34fc4
alex <alexandre.souvorov@edu.esiee-it.fr>
2023-10-13 10:53:55 +0200

    remove unnecessary code in main py

--- main.py
+++ main.py
@@ -120,8 +120,3 @@ app.include_router(culture_router, prefix="/api")
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
